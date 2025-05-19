eee2368ed1f0c8e97356b7ea493f7eaceadb29d4
HeranYimer <heranyimer@gmail.com>
2025-05-19 19:34:05 +0300

    modified

--- netflix-project/src/Components/Banner/Banner.jsx
+++ netflix-project/src/Components/Banner/Banner.jsx
@@ -41,7 +41,7 @@ function Banner() {
         </h1>
         <div className="banner_buttons">
           <button className="banner_button play">Play</button>
-          <button className="banner_button play">My List</button>
+          <button className="banner_button myList">My List</button>
         </div>
         <h1 className="banner_description">{truncate(movie?.overview, 150)}</h1>
       </div>
